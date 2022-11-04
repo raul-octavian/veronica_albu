35a8607a38c7bb5045413b7e284d57ca05957828
RaulOctavian <raul.albu@pitechplus.com>
2022-11-04 06:20:32 +0200

    create pages

---
+++ src/pages/Bookings.tsx
@@ -0,0 +1,9 @@
+const Bookings = () => {
+	return (
+		<div className='prose'>
+			<h1> Bookings</h1>
+		</div>
+	);
+};
+
+export default Bookings;
---
+++ src/pages/Products.tsx
@@ -0,0 +1,9 @@
+const Products = () => {
+	return (
+		<div className='prose'>
+			<h1> Products</h1>
+		</div>
+	);
+};
+
+export default Products;
