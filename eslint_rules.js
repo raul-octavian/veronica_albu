227840950b0f9cf98884a3f86cabf6121e879521
RaulOctavian <raul.albu@pitechplus.com>
2022-11-04 06:17:47 +0200

    project setup

---
+++ eslint_rules.js
@@ -0,0 +1,11 @@
+const rules = {
+  'no-console': ['error', { allow: ['error', 'warn', 'info'] }],
+  '@typescript-eslint/no-var-requires': 'off',
+  'react/prop-types': 'off',
+  'react/display-name': 'off',
+  '@typescript-eslint/no-unused-vars': ['error'],
+  'array-callback-return': 'error',
+  'no-only-tests/no-only-tests': 'error'
+};
+
+module.exports = rules;
