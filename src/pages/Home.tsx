0aea1a5992a5329063b2996329acae3fb8891786
RaulOctavian <raul.albu@pitechplus.com>
2022-11-05 11:03:43 +0200

    make phy logo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,23 @@
+import ArrowDown from '../components/icons/ArrowDown';
+import Logo from '../components/Logo/Logo';
+import PhyLogo from '../components/phyLogo/PhyLogo';
 const Home = () => {
-	return (
-		<div className='prose'>
-			<h1> Home</h1>
-		</div>
-	);
+  return (
+    <>
+      <section className='flex flex-col justify-center pb-24 h-screen'>
+        <div className='items-center flex-1 shrink-1 flex flex-col justify-center'>
+          <Logo />
+        </div>
+        <div className='text-accent-main flex justify-center'>
+          <ArrowDown />
+        </div>
+      </section>
+      <div className='bg-accent-main py-4'>
+        <PhyLogo />
+      </div>
+      
+    </>
+  );
 };
 
 export default Home;
