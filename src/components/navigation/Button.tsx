33cd836716c80e2e2f6bf36854b0d2ab09fbf87d
RaulOctavian <raul.albu@pitechplus.com>
2022-11-13 17:27:29 +0200

    add button dynamic type

--- src/components/navigation/Button.tsx
+++ src/components/navigation/Button.tsx
@@ -2,13 +2,14 @@ import { FC } from 'react';
 
 type ButtonProps = {
   value: string;
+  type?: 'button' | 'submit' | 'reset';
 };
 
-const Button: FC<ButtonProps> = ({ value }) => {
+const Button: FC<ButtonProps> = ({ value, type = 'button' }) => {
   return (
     <button
       className={`text-primary-main bg-accent-main focus:ring-4 focus:ring-accent-soft font-medium font-header rounded-lg text-lg uppercase tracking-widest w-[220px] px-6 py-2 m-2 focus:outline-none transition-all duration-300 hover:tracking-wide`}
-      type='button'
+      type={type}
     >
       {value}
     </button>
